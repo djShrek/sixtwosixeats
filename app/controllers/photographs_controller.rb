fdbfc4bce56a5de75f3758d341382fdf6f963307
djShrek <boho527@gmail.com>
2015-06-22 22:42:41 -0700

    added devise

--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -1,6 +1,7 @@
 class PhotographsController < ApplicationController
   def index
-    @photograph ||= Photograph.get_photos
+    @photograph ||= Photograph.get_photos(params[:instagram_url])
+    @pagination = @photograph["pagination"]["next_url"]
     @photographs = @photograph["data"]
   end
 end
--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -1,12 +1,16 @@
 require 'open-uri'
 
 class Photograph
-  def self.get_photos
+  def self.get_photos(instagram_url)
     # gem that does HTTP requests
-    hash_tag = "626eats"
-    instagram_url = "https://api.instagram.com/v1/tags/#{hash_tag}/media/recent?client_id=#{ENV["INSTAGRAM_CLIENT_ID"]}"
+    if instagram_url.nil?
+      hash_tag = "626eats"
+      instagram_url = "https://api.instagram.com/v1/tags/#{hash_tag}/media/recent?client_id=#{ENV["INSTAGRAM_CLIENT_ID"]}"
+    else
+      instagram_url = instagram_url
+    end
     data = URI.parse(instagram_url).read
-    @data ||= JSON.parse(data)
+    JSON.parse(data)
   end
 
   def self.photograph_data
