e91cfa2b7b3b9bfce8f629fb71c93516cfd0ef34
djShrek <boho527@gmail.com>
2015-06-24 02:08:45 -0700

    added instagram, figaro gem

--- Gemfile
+++ Gemfile
@@ -26,7 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'figaro'
 gem 'devise'
 gem 'omniauth'
-gem 'omniauth-instagram'
+gem 'instagram'
+gem 'figaro'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -1,7 +1,7 @@
 class PhotographsController < ApplicationController
   def index
     @photograph ||= Photograph.get_photos(params[:instagram_url])
-    @pagination = @photograph["pagination"]["next_url"]
-    @photographs = @photograph["data"]
+    @pagination   = @photograph["pagination"]["next_url"]
+    @photographs  = @photograph["data"]
   end
 end
